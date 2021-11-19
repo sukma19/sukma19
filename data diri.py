92afe9d35070d671755407eae1cc6964e4c5e471
sukma19 <94689995+sukma19@users.noreply.github.com>
2021-11-19 19:42:06 +0700

    Add files via upload

---
+++ data diri.py
@@ -0,0 +1,11 @@
+nama="Sukma Maulana Indrajid"
+nim=212410102077
+prodi="Teknologi Informasi"
+angkatan=2021
+asal="Lumajang"
+
+print("Nama     :",nama)
+print("NIM      :",nim)
+print("Prodi    :",prodi)
+print("Angkatan :",angkatan)
+print("Asal     :",asal)
